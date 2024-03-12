452228f8b1113d54d84df7349289711d7d1cd53d
ccadran <clariocadran33@gmail.com>
2024-03-12 14:07:20 +0100

    header

--- src/app/components/Header/header.jsx
+++ src/app/components/Header/header.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import styles from "./header.module.scss";
 
 export default function header() {
-  return <div className={styles.header}>header</div>;
+  return (
+    <div className={styles.header}>
+      <ul>
+        <a href="">
+          <li>Shop</li>
+        </a>
+        <a href="">
+          <li>Galeries</li>
+        </a>
+        <a href="">
+          <li>Le club</li>
+        </a>
+        <a href="">
+          <li>Outfit</li>
+        </a>
+      </ul>
+    </div>
+  );
 }
