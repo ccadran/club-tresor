c0c7b1d7282666680832a1baae0ad97e9d837b0f
ccadran <clariocadran33@gmail.com>
2024-03-14 17:18:30 +0100

    header anim done

--- src/app/components/Header/header.jsx
+++ src/app/components/Header/header.jsx
@@ -1,23 +1,62 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./header.module.scss";
 
 export default function Header() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState("up");
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  useEffect(() => {
+    const handleScrollDirection = () => {
+      const currentScrollPosition = window.scrollY;
+      if (currentScrollPosition > scrollPosition) {
+        setScrollDirection("down");
+      } else {
+        setScrollDirection("up");
+      }
+      setScrollPosition(currentScrollPosition);
+    };
+
+    window.addEventListener("scroll", handleScrollDirection);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollDirection);
+    };
+  }, [scrollPosition]);
   return (
-    <div className={styles.header}>
-      <ul>
-        <a href="">
-          <li>Shop</li>
-        </a>
-        <a href="">
-          <li>Galeries</li>
-        </a>
-        <a href="">
-          <li>Le club</li>
-        </a>
-        <a href="">
-          <li>Outfit</li>
-        </a>
-      </ul>
+    <div className={styles.headerContainer}>
+      <div
+        className={`${styles.header} ${
+          scrollDirection === "down" ? styles.hide : styles.show
+        }`}
+      >
+        {" "}
+        <ul>
+          <a href="">
+            <li>Shop</li>
+          </a>
+          <a href="">
+            <li>Galeries</li>
+          </a>
+          <a href="">
+            <li>Le club</li>
+          </a>
+          <a href="">
+            <li>Outfit</li>
+          </a>
+        </ul>
+      </div>
     </div>
   );
 }
