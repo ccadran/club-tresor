97f8e2bb236f728a87ca4206447948b4eb76b94e
ccadran <clariocadran33@gmail.com>
2024-03-15 17:30:41 +0100

    change import

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@
 
 import Image from "next/image";
 import styles from "./page.module.scss";
-import Concept from "./components/Concept/Concept";
-import Hero from "./components/Hero/Hero";
-import Engagement1 from "./components/Engagement1/Engagement1";
-import Engagement2 from "./components/Engagement2/Engagement2";
-import About from "./components/About/About";
+import Concept from "./components/Concept/Concept.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Engagement1 from "./components/Engagement1/Engagement1.jsx";
+import Engagement2 from "./components/Engagement2/Engagement2.jsx";
+import About from "./components/About/About.jsx";
 
 export default function Home() {
   return (
