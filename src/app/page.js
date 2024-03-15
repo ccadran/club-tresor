acbd12d10b5bf8c4ee6a4d205f71f4287091f3c1
ccadran <clariocadran33@gmail.com>
2024-03-15 17:48:55 +0100

    test edit concept import

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.scss";
-import Concept from "./components/Concept/Concept.jsx";
+import Concept from "./components/ConceptTest/Concept.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Engagement1 from "./components/Engagement1/Engagement1.jsx";
 import Engagement2 from "./components/Engagement2/Engagement2.jsx";
