ffe5cebd2eec69c525e9e5fa1c2ed7a7e93078e7
ccadran <clariocadran33@gmail.com>
2024-03-15 09:03:00 +0100

    create the hero timeline

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,8 +1,31 @@
-import React, { useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styles from "./hero.module.scss";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Hero() {
   const imageRef = useRef();
+  const spaceRef = useRef();
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: spaceRef.current,
+        scrub: true,
+        start: "top top",
+        end: "bottom top",
+        markers: true,
+      },
+    });
+
+    timeline.to(imageRef.current, {
+      opacity: 0.5,
+      width: "100%",
+      height: "100vh",
+    });
+  }, []);
 
   return (
     <div className={styles.hero}>
@@ -11,7 +34,7 @@ export default function Hero() {
         <p>Parce que votre amour est indémodable</p>
       </div>
       <div className={styles.heroAnimations}>
-        <div className={styles.space}></div>
+        <div ref={spaceRef} className={styles.space}></div>
         <div ref={imageRef} className={styles.heroIllustrations}>
           <img src="/images/illu.png" alt="" />
         </div>
